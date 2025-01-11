195e45e22e3f07a21599fe8de83f710f997f8ca9
Niklas Mollenhauer <nikeee@outlook.com>
2025-01-11 17:23:41 +0100

    Fix parameter usage

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@
 
 import { aesCmac as _aesCmac } from "./aes-cmac.js";
 
-export function aesCmac(key, message, options) {
+export function aesCmac(key, message, options = {}) {
 	validateKey(key);
 	const messageBuffer = validateMessage(message);
-	options = options ? options : {};
 	const result = _aesCmac(key, messageBuffer);
 	return options.returnAsBuffer ? result : result.toString("hex");
 }
