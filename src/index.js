a89400c393a2109f38a0148d4f90442b51aaf224
Niklas Mollenhauer <nikeee@outlook.com>
2025-01-12 17:25:14 +0100

    Re-export core function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 // @ts-check
-
-import { aesCmac as _aesCmac } from "./aes-cmac.js";
-
-/**
- * @param {Buffer} key
- * @param {Buffer} message
- * @returns {Buffer}
- */
-export default function aesCmac(key, message) {
-	return _aesCmac(key, message);
-}
+export { aesCmac as default } from "./aes-cmac.js";
