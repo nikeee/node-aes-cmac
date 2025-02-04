cea547530eda65827767bf3c42582257e66cb5a8
Niklas Mollenhauer <nikeee@outlook.com>
2025-02-04 15:53:09 +0100

    Add benchmark infra

---
+++ src/benchmark/index.js
@@ -0,0 +1,25 @@
+// @ts-check
+import { Bench } from "tinybench";
+
+import aesCmac from "../index.js";
+
+const bench = new Bench({ name: "performance", time: 100 });
+
+const keys = new Array(100).fill(0).map((_, index) => Buffer.alloc(16, index));
+const messages = new Array(100)
+	.fill(0)
+	.map((_, index) => Buffer.alloc(index % 2 === 0 ? 16 : 32, index * 2));
+
+bench.add("mac generation", () => {
+	for (let k = 0; k < keys.length; ++k) {
+		const key = keys[k];
+		for (let m = 0; m < messages.length; ++m) {
+			const cmac = aesCmac(key, messages[m]);
+		}
+	}
+});
+
+await bench.run();
+
+console.log(bench.name);
+console.table(bench.table());
